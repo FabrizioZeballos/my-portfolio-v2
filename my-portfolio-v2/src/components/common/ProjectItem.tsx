da35a02c8d10653081c59b1abf4d7904999a0551
Fabrizio Zeballos <zeballosfab@gmail.com>
2026-01-06 13:59:39 -0300

    Improve projects section styles

--- my-portfolio-v2/src/components/common/ProjectItem.tsx
+++ my-portfolio-v2/src/components/common/ProjectItem.tsx
@@ -19,11 +19,19 @@ export function ProjectItem({
   links,
 }: ProjectItemProps) {
   return (
-    <div className="flex">
-      <img src={image} alt="" className="flex-1" />
-      <div className="pl-20 py-4 flex flex-col flex-1 justify-between">
+    <div className="flex justify-center items-center">
+      <div className="flex-1 h-full w-full">
+        <img
+          src={image}
+          alt=""
+          className="h-full w-full object-cover rounded-[20px]"
+        />
+      </div>
+      <div className="pl-18 flex flex-col flex-1">
         <div className="flex justify-between">
-          <h4 className="text-white-secondary">{title}</h4>
+          <h4 className="text-xl text-white-secondary font-semibold">
+            {title}
+          </h4>
           <div className="flex gap-4">
             {links?.map((link, index) => (
               <a
@@ -38,12 +46,12 @@ export function ProjectItem({
             ))}
           </div>
         </div>
-        <p className="text-gray-secondary">{description}</p>
+        <p className="my-2 text-gray-secondary">{description}</p>
         <div className="flex gap-2 mt-4">
           {techStack.map((tech) => (
             <span
               key={tech}
-              className="text-[10px] uppercase tracking-tighter text-white-primary px-2 py-1 rounded bg-background"
+              className="text-xs text-white-primary px-2 py-1 rounded bg-background"
             >
               {tech}
             </span>
--- my-portfolio-v2/src/components/sections/Projects.tsx
+++ my-portfolio-v2/src/components/sections/Projects.tsx
@@ -1,44 +1,46 @@
 import { CardHeader } from "../common/CardHeader";
 import { ProjectItem } from "../common/ProjectItem";
-import projectImg from "../../assets/experience-1.png";
+import project1 from "../../assets/project-1.png";
+import project2 from "../../assets/project-2.png";
+import project3 from "../../assets/project-3.png";
 import { FaGithub, FaPlayCircle } from "react-icons/fa";
 
 export function Projects() {
   return (
-    <div className="hidden md:block py-40 border border-red-500">
-      <div className="p-20 flex flex-col bg-[#141415] text-white gap-20">
+    <div className="hidden md:block md:bg-card md:py-26 md:px-30 border-0 md:border-2 border-white/4 rounded-[40px]">
+      <div className="flex flex-col bg-[#141415] text-white gap-20">
         <CardHeader header={"pet projects"} />
         <ProjectItem
-          image={projectImg}
+          image={project1}
           title={"hhbc bank"}
           description={
             "an appointment scheduling app for a bank aiming to enhance customer service where users can register, log in, schedule, and cancel appointments seamlessly and intuitively"
           }
-          techStack={["express", "node"]}
+          techStack={["express", "react", "redux", "postgresql"]}
           links={[
             { url: "https://github.com/...", icon: FaGithub },
             { url: "https://github.com/...", icon: FaPlayCircle },
           ]}
         />
         <ProjectItem
-          image={projectImg}
-          title={"hhbc bank"}
+          image={project2}
+          title={"coreforce fitness"}
           description={
-            "an appointment scheduling app for a bank aiming to enhance customer service where users can register, log in, schedule, and cancel appointments seamlessly and intuitively"
+            "a gym application designed to make a significant impact on the fitness industry where users can interact with trainers in real-time via chat, purchase memberships, and access an ai-powered chatbot"
           }
-          techStack={["express", "node"]}
+          techStack={["nestjs", "react", "tailwind", "postgresql"]}
           links={[
             { url: "https://github.com/...", icon: FaGithub },
             { url: "https://github.com/...", icon: FaPlayCircle },
           ]}
         />
         <ProjectItem
-          image={projectImg}
-          title={"hhbc bank"}
+          image={project3}
+          title={"ecommerce backend"}
           description={
-            "an appointment scheduling app for a bank aiming to enhance customer service where users can register, log in, schedule, and cancel appointments seamlessly and intuitively"
+            "an ecommerce app designed to manage products, users, and orders with a robust backend built on a restful api that integrates seamlessly with other systems for a smooth user experience"
           }
-          techStack={["express", "node"]}
+          techStack={["nestjs", "postgresql"]}
           links={[
             { url: "https://github.com/...", icon: FaGithub },
             { url: "https://github.com/...", icon: FaPlayCircle },
