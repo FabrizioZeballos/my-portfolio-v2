4e4830ede14d5f49219241c6c2a66c822b576c2e
Fabrizio Zeballos <zeballosfab@gmail.com>
2026-01-06 12:47:12 -0300

    Improve about styles for mobile

--- my-portfolio-v2/src/components/common/CardHeader.tsx
+++ my-portfolio-v2/src/components/common/CardHeader.tsx
@@ -4,7 +4,7 @@ type CardHeaderProps = {
 
 export function CardHeader({ header }: CardHeaderProps) {
   return (
-    <h2 className="text-3xl sm:text-4xl md:text-5xl text-white-primary">
+    <h2 className="text-3xl sm:text-4xl md:text-5xl text-white-primary font-jakarta font-semibold">
       / {header}
     </h2>
   );
--- my-portfolio-v2/src/components/sections/About.tsx
+++ my-portfolio-v2/src/components/sections/About.tsx
@@ -3,66 +3,72 @@ import { CardHeader } from "../common/CardHeader";
 
 export function About() {
   return (
-    <div className="border border-green-500">
+    <div className="px-4 md:px-0">
       <div className="w-full flex flex-col md:flex-row-reverse md:bg-[#141415] md:text-white md:py-10 md:px-10">
         <div className="flex flex-1">
-          <img src={aboutImg} alt="" />
+          <img src={aboutImg} alt="" className="rounded-[40px] mb-14 md:mb-0" />
         </div>
         <div className="flex flex-col flex-1">
           <CardHeader header={"about me"} />
-          <div className="text-gray-secondary">
-            <p>
+          <div className="text-gray-secondary font-sans font-light">
+            <p className="my-6">
               <span>
                 currently a full-stack developer at{" "}
-                <span className="text-white-secondary">b/ackcode_,</span>
+                <span className="text-white-secondary font-normal">
+                  b/ackcode_,
+                </span>
               </span>
               <br />
               <span>a swiss-based software startup</span>
             </p>
-            <p>
+            <p className="my-6">
               <span>there, I built react features for an in-person</span>
               <br />
               <span>
                 luxury watch configurator for{" "}
-                <span className="text-white-secondary">audemars piguet</span>
+                <span className="text-white-secondary font-normal">
+                  audemars piguet
+                </span>
               </span>
               <br />
               <span>and contributed to an ai assistant using llms,</span>
               <br />
               <span>python, langgraph, mcps, and electron</span>
             </p>
-            <p>
+            <p className="my-6">
               <span>studying law on the side, now in my 4th year at</span>
               <br />
-              <span className="text-white-secondary">
+              <span className="text-white-secondary font-normal">
                 national university of cuyo
               </span>
             </p>
-            <p>
+            <p className="my-6">
               <span>here are some technologies i've been working</span>
               <br />
               <span>with:</span>
               <br />
-              <div>
-                <div>
+              <div className="mt-6 flex flex-col gap-4">
+                {" "}
+                {/* Vertical gap between rows */}
+                <div className="flex gap-8">
+                  {" "}
+                  {/* Horizontal gap between items */}
                   <span>
                     <span className="text-white-secondary">-</span> react.js
-                  </span>{" "}
+                  </span>
                   <span>
                     <span className="text-white-secondary">-</span> python
                   </span>
                 </div>
-                <br />
-                <div>
+                <div className="flex gap-8">
                   <span>
                     <span className="text-white-secondary">-</span> javascript
-                  </span>{" "}
+                  </span>
                   <span>
                     <span className="text-white-secondary">-</span> node.js
                   </span>
                 </div>
-                <br />
-                <div>
+                <div className="flex gap-8">
                   <span>
                     <span className="text-white-secondary">-</span> typescript
                   </span>
@@ -72,7 +78,7 @@ export function About() {
                 </div>
               </div>
             </p>
-            <p>
+            <p className="hidden md:block my-6">
               <span>when i'm not building interfaces or debugging</span>
               <br />
               <span>apis, i'm out exploring places like this 📸</span>
